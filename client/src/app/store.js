e3c0c2a1697d41efcd9b7f92db964772b201e5cd
Hoang Thien <thien349k@gmail.com>
2024-11-05 21:16:22 +0700

    add client

---
+++ client/src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import roomReducer from "../features/room/roomSlice";
+export const store = configureStore({
+  reducer: {
+    room: roomReducer,
+  },
+});
