8759bef9bea4b820c202081c542b63fb89da2adb
Hoang Thien <thien349k@gmail.com>
2024-11-12 21:50:49 +0700

    add Chat

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Rooms from "./Pages/Rooms/Rooms";
 import Room from "./Pages/Room/Room";
 import Header from "./component/Header/Header";
 import Booking from "./Pages/Booking/Booking";
+import Chat from "./Pages/Chat/Chat";
 const App = () => {
   return (
     <div>
@@ -15,6 +16,8 @@ const App = () => {
           <Route path="/rooms" element={<Rooms />} />
           <Route path="/rooms/all/:id" element={<Room />} />
           <Route path="/bookings/:id" element={<Booking />} />
+          <Route path="/bookings/:id" element={<Booking />} />
+          <Route path="/chat" element={<Chat />} />
 
         </Routes>
       </Router>
---
+++ client/src/Pages/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+const Chat = () => {
+    return (
+        <div>
+            <h1 className="heading center">Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
