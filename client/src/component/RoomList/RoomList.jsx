689ca7ce6674dd2871628b4b6271d6c32d5f5d5e
Hoang Thien <thien349k@gmail.com>
2024-11-22 13:34:07 +0700

    send emaill

--- client/src/component/RoomList/RoomList.jsx
+++ client/src/component/RoomList/RoomList.jsx
@@ -5,19 +5,21 @@ import Carousel from "../Carousel/Carousel";
 
 const RoomList = ({ data }) => {
     return (
-        <div id="room-list">
-            {data.map((item, index) => {
-                return (
+        <div className="container">
+            <div id="room-list">
+                {data.map((item, index) => {
+                    return (
 
-                    <Link to={`/rooms/all/${item._id}`} key={item.id} className="room-unit">
-                        <div className="img-wrapper">
-                            <Carousel data={item.img} />
-                        </div>
-                        <p className="name"> {item.name}</p>
-                    </Link>
-                );
+                        <Link to={`/rooms/all/${item._id}`} key={item.id} className="room-unit">
+                            <div className="img-wrapper">
+                                <Carousel data={item.img} />
+                            </div>
+                            <p className="name"> {item.name}</p>
+                        </Link>
+                    );
 
-            })}
+                })}
+            </div>
         </div>
     )
 }
